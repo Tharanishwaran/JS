501ec0295c839fd5f0b4ac1d45f182e5cb640d7a
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2023-12-05 21:43:49 +0530

    Create ifelse.js

---
+++ ifelse.js
@@ -0,0 +1,15 @@
+let number = 10; //declaring and initalizing the variable
+
+//using if else to find the number is even or odd
+
+if (number % 2 === 0) { 
+
+  console.log(number + ' is even');
+
+}
+
+else {
+
+  console.log(number + ' is odd');
+  
+}
